afee924ee35479e1197e9a77c9610bdf797c97d0
Dmytro Tashpikov <neodim84@gmail.com>
2022-10-28 23:46:01 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as basicLightbox from 'basiclightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
