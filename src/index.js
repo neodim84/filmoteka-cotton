f0484fb8fd0e0add3cefa900cb3b602218d3340c
Dmytro Tashpikov <neodim84@gmail.com>
2022-10-29 23:28:48 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import axios from 'axios';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 
 import renderPagination from './js/pagination';
+import spinnerStart from './js/spinner';
