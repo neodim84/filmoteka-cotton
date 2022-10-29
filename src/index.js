d9e3ead7af0011fed05061db3d0fc50f97d2272c
Dmytro Tashpikov <neodim84@gmail.com>
2022-10-29 19:29:22 +0300

    разметка

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import * as basicLightbox from 'basiclightbox';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+
+import renderPagination from './js/pagination';
