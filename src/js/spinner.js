31a6ace8d3de653cf51dd9404c8184a2015f702b
Vitalii <svitalii138@gmail.com>
2022-10-29 22:23:44 +0300

    Debounce changed to 1000ms

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -8,8 +8,8 @@ export const spinnerStart = function () {
 
 export const spinnerStop = _(() => {
   spinnerRef.classList.add('is-hidden');
-}, 500);
+}, 1000);
 
 // 1. import { spinnerStart, spinnerStop } from './js/spinner';
 // 2. spinnerStart(); - spinner is visible;
-// 3. spinnerStop() - spinner is hidden (debounce 500ms);
+// 3. spinnerStop() - spinner is hidden (debounce 1000ms);
