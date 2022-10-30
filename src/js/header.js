4b4d3d6ce37ce5bb8afc66102e055ba991710569
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-10-30 23:15:12 +0200

    import genreTitle function

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import genres from '../json/genres.json';
-
-console.log(genres);
+import { genreTitle } from './genresSelect';
 
 const formRef = document.querySelector('.header__form');
 const cardsListRef = document.querySelector('.film-gallery__list');
@@ -81,19 +80,3 @@ export function createMarkup(hits) {
     })
     .join('');
 }
-
-export const genreTitle = (genreID, genres) => {
-  const filter = genres.filter(genre => genreID.includes(genre.id));
-  const map = filter.map(item => item.name);
-  const arr = [];
-  for (let index = 0; index <= map.length; index++) {
-    const element = map[index];
-    arr.push(element);
-  }
-
-  if (arr.length > 2) {
-    return (arrToShow = arr.slice(0, 3).join(', ').concat(', Other'));
-  }
-
-  return (arrToShow = arr.slice(0, arr.length).join(', '));
-};
