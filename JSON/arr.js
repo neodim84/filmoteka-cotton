af146f17292bdd2e6164b1afd372046c4fd3b49d
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-10-30 12:24:31 +0200

    changes

--- JSON/arr.js
+++ JSON/arr.js
@@ -1,89 +1,85 @@
 [
   {
-    "name": "Alekhandro_Lago",
-    "foto_url": "../src/images/students/Alekhandro_Lago.jpg",
-    "github": "https://github.com/Alekhandro24",
-    "telegramm": "#",
-    "linkidin": "#"
+    "name": "Dmytro_Tashpikov",
+    "foto_url": "../src/images/students/Dmytro_Tashpikov.jpg",
+    "github": "https://github.com/neodim84",
+    "role": "Team Lead"
   },
 
   {
     "name": "Alla_Yukhymenko",
     "foto_url": "../src/images/students/Alla_Yukhymenko.jpeg",
     "github": "https://github.com/AllaYukhym",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Scrum Master"
+  },
+
+  {
+    "name": "Alekhandro_Lago",
+    "foto_url": "../src/images/students/Alekhandro_Lago.jpg",
+    "github": "https://github.com/Alekhandro24",
+    "role": "Developer"
   },
 
   {
     "name": "Arthur_S",
     "foto_url": "../src/images/students/Arthur_S.jpg",
     "github": "https://github.com/Arthurosh",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   },
 
   {
     "name": "Dima_Viatkovskiy",
     "foto_url": "../src/images/students/Dima_Viatkovskiy.jpg",
     "github": "https://github.com/dima2605",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   },
 
   {
     "name": "Dmytro_Tashpikov",
     "foto_url": "../src/images/students/Dmytro_Tashpikov.jpg",
     "github": "https://github.com/neodim84",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Team Lead"
   },
 
   {
     "name": "Ievgen_Vorontsov",
     "foto_url": "../src/images/students/Ievgen_Vorontsov.jpeg",
     "github": "https://github.com/Vorontsov1",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   },
 
   {
     "name": "Khrystyna",
     "foto_url": "../src/images/students/Khrystyna.jpg",
     "github": "https://github.com/Khryyystyna",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   },
 
   {
     "name": "Oleksandr_Diachenko",
     "foto_url": "../src/images/students/Oleksandr_Diachenko.jpg",
     "github": "https://github.com/Pallich",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   },
 
   {
     "name": "Oleksandr_Litvin",
     "foto_url": "../src/images/students/Oleksandr_Litvin.jpeg",
     "github": "https://github.com/Oleksandr-Lytvyn",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   },
 
   {
     "name": "Vitalii_Sapunov",
     "foto_url": "../src/images/students/Vitalii_Sapunov.jpg",
     "github": "https://github.com/Vi7ality",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   },
 
   {
     "name": "Volodymyr_Kurov",
     "foto_url": "../src/images/students/Volodymyr_Kurov.jpeg",
     "github": "https://github.com/vokur13",
-    "telegramm": "#",
-    "linkidin": "#"
+    "role": "Developer"
   }
 ]
