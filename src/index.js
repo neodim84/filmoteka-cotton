e7b7cec16f082aac58cdb6140e21641b2c235f5d
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-10-30 10:25:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import * as basicLightbox from 'basiclightbox';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
-import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+// import * as basicLightbox from 'basiclightbox';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import axios from 'axios';
+// import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 
-import renderPagination from './js/pagination';
-import spinnerStart from './js/spinner';
+// import renderPagination from './js/pagination';
+// import spinnerStart from './js/spinner';
