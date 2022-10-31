3cbc4ad438ded6e38c8a6d323a49b48022e7e657
Dmytro Tashpikov <neodim84@gmail.com>
2022-10-31 18:07:16 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-// import * as basicLightbox from 'basiclightbox';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import axios from 'axios';
-// import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
-
 import './js/header';
 import './js/gallery';
 import './js/genresSelect';
 import './js/pagination';
-// import { refs } from './js/refs';
 import './js/header';
 import './js/gallery';
 import './js/modal-main';
 import './js/pagination';
-// import { spinnerStart, spinnerStop } from './js/spinner';
 import './js/footer';
