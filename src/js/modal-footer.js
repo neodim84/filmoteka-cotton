a6a81df2fcd14020dcb5eb05df6889970ce5708c
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-10-31 18:02:10 +0200

    2

--- src/js/modal-footer.js
+++ src/js/modal-footer.js
@@ -1,1 +1,28 @@
 
+
+
+const openLink = () => {
+  window.addEventListener('keydown', onKeyPress);
+  backdropStEl.classList.remove('is-hidden');
+};
+
+const closeModalStud = () => {
+  backdropStEl.classList.add('is-hidden');
+  window.removeEventListener('keydown', onKeyPress);
+};
+
+linkToDev.addEventListener('click', openLink);
+closeModalBtn.addEventListener('click', closeModalStud);
+backdropStEl.addEventListener('click', onBackdropClick);
+
+function onKeyPress(event) {
+  if (event.code === 'Escape') {
+    closeModalStud();
+  }
+}
+
+function onBackdropClick(e) {
+  if (e.target === e.currentTarget) {
+    closeModalStud();
+  }
+}
