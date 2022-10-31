3d90aa405a9a8cd8be6c62e05a6195fb9ac174e1
Dmytro Tashpikov <neodim84@gmail.com>
2022-10-31 20:45:12 +0200

    refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,12 +1,4 @@
-// export const refs = {
-//   body: document.querySelector('body'),
-//   cardMovie: document.querySelector('.film-gallery__list'),
-//   modalList: document.querySelector('.modal__list'),
-//   modal: document.querySelector('[data-modal]'),
-//   closeModalBtn: document.querySelector('[data-modal-close]'),
-// };
-
-const refs = {
+export const refs = {
   body: document.querySelector('body'),
   cardMovie: document.querySelector('.film-gallery__list'),
   modalList: document.querySelector('.modal__list'),
@@ -14,4 +6,12 @@ const refs = {
   closeModalBtn: document.querySelector('[data-modal-close]'),
 };
 
-export const { body, cardMovie, modalList, modal, closeModalBtn } = refs;
+// const refs = {
+//   body: document.querySelector('body'),
+//   cardMovie: document.querySelector('.film-gallery__list'),
+//   modalList: document.querySelector('.modal__list'),
+//   modal: document.querySelector('[data-modal]'),
+//   closeModalBtn: document.querySelector('[data-modal-close]'),
+// };
+
+// export const { body, cardMovie, modalList, modal, closeModalBtn } = refs;
