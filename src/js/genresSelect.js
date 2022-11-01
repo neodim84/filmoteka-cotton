fb0635eda67e3d17180069853c150ac1080d54ae
vokur13 <vokur13@gmail.com>
2022-11-01 10:41:16 +0200

    Update genresSelect.js

--- src/js/genresSelect.js
+++ src/js/genresSelect.js
@@ -1,15 +1,19 @@
-export const genreTitle = (genreID, genres) => {
-  const filter = genres.filter(genre => genreID.includes(genre.id));
-  const map = filter.map(item => item.name);
-  const arr = [];
-  for (let index = 0; index <= map.length; index++) {
-    const element = map[index];
-    arr.push(element);
-  }
+export const genreTitle = async (genreID, genres) => {
+  try {
+    const filter = await genres.filter(genre => genreID.includes(genre.id));
+    const map = await filter.map(item => item.name);
+    const arr = [];
+    for (let index = 0; index <= map.length; index++) {
+      const element = map[index];
+      arr.push(element);
+    }
 
-  if (arr.length > 2) {
-    return (arrToShow = arr.slice(0, 2).join(', ').concat(', Other'));
-  }
+    if (arr.length > 2) {
+      return (arrToShow = arr.slice(0, 2).join(', ').concat(', Other'));
+    }
 
-  return (arrToShow = arr.slice(0, arr.length - 1).join(', '));
+    return (arrToShow = arr.slice(0, arr.length - 1).join(', '));
+  } catch (error) {
+    console.log(error);
+  }
 };
