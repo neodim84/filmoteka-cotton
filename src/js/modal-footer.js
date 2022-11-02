0c460a9e184f4a1f80c3e21485331ddf19b63e2c
vokur13 <vokur13@gmail.com>
2022-11-02 19:23:39 +0200

    img

--- src/js/modal-footer.js
+++ src/js/modal-footer.js
@@ -1,26 +1,50 @@
-const studentsModalBtn = document.querySelector('.students-modal');
-const modalWindow = document.querySelector('.backdrop');
-const modalClose = document.querySelector('.button-modal');
+import team from '../json/team.json';
+import { teamMarkUp } from './createTeamMarkup';
 
+const studentsModalBtn = document.querySelector('.students-modal');
+const modalWindow = document.querySelector('.backdrop-footer');
+const modalClose = document.querySelector('.team-modal__close-btn');
+const teamList = document.querySelector('.team__list');
 
 studentsModalBtn.addEventListener('click', onModalOpen);
 modalClose.addEventListener('click', onModalClose);
 
-
 function onModalOpen(e) {
-    modalWindow.classList.remove('is-hidden');
-} 
+  e.preventDefault();
+  //   teamList.innerHTML = '';
+  //   teamList.innerHTML = teamMarkUp(team);
+  modalWindow.classList.remove('is-hidden');
 
-function onModalClose(e) {
-    modalWindow.classList.add('is-hidden');
-} 
-   
-
-
-
-
-function onKeyPress(e) {
-  if (e.code === 'Escape') {
-    onModalClose();
+  document.addEventListener('keydown', onKeyPress);
+  function onKeyPress(e) {
+    if (e.code === 'Escape') {
+      onModalClose();
+      document.removeEventListener('keydown', onKeyPress);
+    }
   }
 }
+
+function onModalClose(e) {
+  modalWindow.classList.add('is-hidden');
+}
+
+// const studentsModalBtn = document.querySelector('.students-modal');
+// const modalWindow = document.querySelector('.backdrop');
+// const modalClose = document.querySelector('.button-modal');
+
+// studentsModalBtn.addEventListener('click', onModalOpen);
+// modalClose.addEventListener('click', onModalClose);
+
+// function onModalOpen(e) {
+//   modalWindow.classList.remove('is-hidden');
+// }
+
+// function onModalClose(e) {
+//   modalWindow.classList.add('is-hidden');
+// }
+
+// function onKeyPress(e) {
+//   if (e.code === 'Escape') {
+//     onModalClose();
+//   }
+// }
