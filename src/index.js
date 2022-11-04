62ec1f406069e107ef9c61691b15b067532ae94c
Dmytro Tashpikov <neodim84@gmail.com>
2022-11-04 15:14:19 +0200

    index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './js/header';
 import './js/gallery';
 import './js/genresSelect';
 import './js/modal-main';
-// import './js/footer';
+import './js/pagination';
 import './js/modal-footer';
 import './utils/storage';
